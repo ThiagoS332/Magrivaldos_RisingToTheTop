9f3bffe2e7a617b1ab93bab3a930abc412b15fa2
Thiago Soares <thiago.soarescds@gmail.com>
2022-05-06 01:49:20 -0300

    Limiting Player Force
    Added a maximum force that can be applied to the player.
    Still needs to normalize the vector, however using the function/attribute ".normalized" does not fix the vector, it breaks it as the vector goes from 0 to 1

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -7,11 +7,14 @@ public class Goal : MonoBehaviour
 
     private GameObject ballObj = null;
 
+    private Rigidbody2D ballRB;
+
     // Start is called before the first frame update
     void Start()
     {
         if (ballObj == null) {
             ballObj = GameObject.Find("Ball");
+            ballRB = ballObj.GetComponent<Rigidbody2D>();
         }
     }
 
@@ -21,10 +24,22 @@ public class Goal : MonoBehaviour
         
     }
 
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        Debug.Log("depois da espera");
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
+        StartCoroutine(ExecuteAfterTime(10));
+
         Debug.Log("GOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOL da alemanha");
 
         ballObj.transform.position = new Vector3(0f, 0f, 0f);
+
+        ballRB.velocity = new Vector2(0f, 0f);
+        ballRB.angularVelocity = 0f;
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -31,18 +31,6 @@ public class Player : MonoBehaviour
         MovePlayer();
     }
 
-    private bool CheckDragDistanceFromPlayer(float MaxDistance)
-    {
-        if((Input.mousePosition.x >= playerObj.transform.position.x - MaxDistance || Input.mousePosition.x <= playerObj.transform.position.x + MaxDistance) &&
-            (Input.mousePosition.y >= playerObj.transform.position.y - MaxDistance || Input.mousePosition.y <= playerObj.transform.position.y + MaxDistance)) 
-        {
-            return true;
-        }
-
-        return false;
-        
-    }
-
     private void MovePlayer()
     {
         if(Input.GetMouseButtonDown(0)) {
@@ -57,21 +45,56 @@ public class Player : MonoBehaviour
 
             //Debug.Log("EndingDragPos = " + Input.mousePosition);
 
-            //Debug.Log("Difference = " + difference);
+            Debug.Log("Difference = " + difference);
 
             //playerObj.transform.position += difference;
+
+            //if(difference)
+
+            if(difference.x > 100.0f){
+                Debug.Log("Difference X is above 100");
+                difference.x = 100.0f;
+            }
+            else if(difference.x < -100.0f){
+                Debug.Log("Difference X is lower than -100");
+                difference.x = -100.0f;
+            }
+
+            if(difference.y > 100.0f){
+                difference.y = 100.0f;
+            }
+            else if(difference.y < -100.0f){
+                Debug.Log("Difference Y is lower than -100");
+                difference.y = -100.0f;
+            }
 
             if(Input.GetKey("space")){
                 playerRigidbody.AddForce (new Vector2(2 * difference.x, 2 * difference.y));
             }
             else{
-                playerRigidbody.AddForce (new Vector2(difference.x, difference.y));
+                //Debug.Log("Difference = " + difference.normalized);
+                playerRigidbody.AddForce (new Vector3(difference.x, difference.y));
             }
             
 
             //Debug.Log("NewPlayerPos = " + playerObj.transform.position);
         }
 
+    }
+
+    // Doesn't work because after the player is deactivated it cannot be activated again as the this script stops running
+    private void ChangeStates()
+    {
+        if(Input.GetKeyDown("d")){
+            playerObj.SetActive(true);
+            Debug.Log("Ativei o player");
+        }
+
+        if(Input.GetKeyUp("d")){
+            playerObj.SetActive(false);
+            Debug.Log("Desativei o player");
+        }
+        
     }
 
 }
